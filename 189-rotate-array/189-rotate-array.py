bc5143b9afa69e2e978c12a0d659dfc49ed295a7
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-02 20:39:24 +0900

    Time: 527 ms (37.70%), Space: 25.4 MB (75.35%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,13 +1,8 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.????????
-        """
-        # print(nums,k)
         leng = len(nums)
         if k == 0 or k == leng or leng <= 1:
             return nums
         
         k = k%leng
         nums[:] = nums[-k:] + nums[:leng-k]
-        # print(nums)
