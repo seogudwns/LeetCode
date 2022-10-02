bf3f0aaf36958d52d8d967a84823150706e28ad3
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-02 20:38:27 +0900

    Time: 351 ms (67.51%), Space: 25.3 MB (75.35%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -7,6 +7,7 @@ class Solution:
         leng = len(nums)
         if leng <= 1 or k == 0 or k == leng:
             return nums
+        
         k = k%leng
         nums[:] = nums[-k:] + nums[:leng-k]
         # print(nums)
