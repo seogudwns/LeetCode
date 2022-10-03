1704805b169f0c6c4cdd3abd77cfe2cd14cea7cf
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-03 18:01:42 +0900

    Time: 3044 ms (12.82%), Space: 25 MB (52.92%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,33 +1,63 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        leng = len(colors)
-        conse_lst = []
-        curr = colors[0]
-        tmp = []
-        for i in range(leng):
-            if colors[i] != curr:
-                curr = colors[i]
-                conse_lst.append(tmp)
-                tmp = [i]
-            else:
-                tmp.append(i)
+#         leng = len(colors)
+#         conse_lst = []
+#         curr = colors[0]
+#         tmp = []
+#         for i in range(leng):
+#             if colors[i] != curr:
+#                 curr = colors[i]
+#                 conse_lst.append(tmp)
+#                 tmp = [i]
+#             else:
+#                 tmp.append(i)
         
-        if tmp:
-            conse_lst.append(tmp)
+#         if tmp:
+#             conse_lst.append(tmp)
         
-        result = 0
-        for i in conse_lst:
-            if len(i) == 1:
-                continue
+#         result = 0
+#         for i in conse_lst:
+#             if len(i) == 1:
+#                 continue
                 
-            maxi = 0
+#             maxi = 0
+#             subt = 0
+#             for j in i:
+#                 subt += neededTime[j]
+#                 if neededTime[j]>maxi:
+#                     maxi = neededTime[j]
+            
+#             result += subt-maxi
+            
+#         return result
+    # 합치면 더 효율이 좋기는 하지만 귀찮다.. 합치자.. 효율 개똥망이네.
+        def calc_remind_maxi(lst):
+            if len(lst) == 1:
+                return 0
+            
             subt = 0
-            for j in i:
+            maxi = 0
+            for j in lst:
                 subt += neededTime[j]
                 if neededTime[j]>maxi:
                     maxi = neededTime[j]
-            
-            result += subt-maxi
+            return subt-maxi
+        
+        leng = len(colors)
+        curr = colors[0]
+        tmp = [0]
+        result = 0
+        
+        for i in range(1,leng):
+            if colors[i] != curr:
+                curr = colors[i]
+                result += calc_remind_maxi(tmp)
+                tmp = [i]
+                
+            else:
+                tmp.append(i)
+        
+        if len(tmp)>1:
+            result += calc_remind_maxi(tmp)
             
         return result
-    # 합치면 더 효율이 좋기는 하지만 귀찮다.
