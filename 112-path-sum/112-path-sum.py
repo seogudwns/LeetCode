67432b9b1569df26f8efbb3385f434e80697883d
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-04 13:51:25 +0900

    Time: 123 ms (5.06%), Space: 15.1 MB (56.49%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,19 +11,34 @@ class Solution:
         if not root:
             return False
         
-        nums = set()
-        def last_sum(root,node):
-            if targetSum in nums:
-                return
-            if not root.left and not root.right:
-                nums.add(node+root.val)
-                return
+#         nums = set()
+#         def last_sum(root,node):
+#             if targetSum in nums:
+#                 return
+#             if not root.left and not root.right:
+#                 nums.add(node+root.val)
+#                 return
             
-            if root.left:
-                last_sum(root.left,node+root.val)
-            if root.right:
-                last_sum(root.right,node+root.val)
+#             if root.left:
+#                 last_sum(root.left,node+root.val)
+#             if root.right:
+#                 last_sum(root.right,node+root.val)
         
-        last_sum(root,0)
-        # print(nums)
-        return targetSum in nums
+#         last_sum(root,0)
+#         # print(nums)
+#         return targetSum in nums
+        Q = deque([(0,root)])
+        while Q:
+            val,tree = Q.popleft()
+            val += tree.val
+            if not tree.left and not tree.right:
+                if val == targetSum:
+                    return True
+                continue
+            
+            if tree.left:
+                Q.append((val,tree.left))
+            if tree.right:
+                Q.append((val,tree.right))
+        
+        return False
