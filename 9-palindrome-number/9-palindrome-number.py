00c2075b5b4e3884ffd97d34496e6fb87d8ec2fd
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-11 17:15:04 +0900

    Time: 95 ms (69.82%), Space: 13.7 MB (96.29%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        if x != x[::-1]:
+        leng = len(x)
+        # print(x[:leng//2],x[leng-1:(leng-1)//2:-1])
+        if x[:leng//2] != x[leng-1:(leng-1)//2:-1]:
             return False
         
         return True
