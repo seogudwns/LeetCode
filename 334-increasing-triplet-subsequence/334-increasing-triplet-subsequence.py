dd7e80dcc645bd15a9719c3d44e97497b93a53be
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-11 14:51:45 +0900

    Time: 762 ms (75.24%), Space: 24.6 MB (80.42%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -10,4 +10,18 @@ class Solution:
                 big = i
         
         return False
+    
+#         # basic code..
+#         X = Counter(nums)
+#         if len(X.keys()) < 3 or nums == sorted(nums,reverse=True):
+#             return False
         
+#         n = len(nums)
+#         for i in range(n-2):
+#             for j in range(i+1,n-1):
+#                 if nums[i]<nums[j]:
+#                     for k in range(j+1,n):
+#                         if nums[j]<nums[k]:
+#                             return True
+#         return False
+#         # 야 이게 통과가 되네 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
