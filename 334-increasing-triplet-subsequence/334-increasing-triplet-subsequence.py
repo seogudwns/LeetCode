3ee1d03641ab7b8257e80509f7736786ea010cf7
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-11 14:51:34 +0900

    Time: 1590 ms (16.67%), Space: 24.5 MB (95.56%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,19 +1,5 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-#         # basic code..
-#         X = Counter(nums)
-#         if len(X.keys()) < 3 or nums == sorted(nums,reverse=True):
-#             return False
-        
-#         n = len(nums)
-#         for i in range(n-2):
-#             for j in range(i+1,n-1):
-#                 if nums[i]<nums[j]:
-#                     for k in range(j+1,n):
-#                         if nums[j]<nums[k]:
-#                             return True
-#         return False
-#         # 야 이게 통과가 되네 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
         small, big = float('inf'),float('inf')
         for i in nums:
             if small > i:
