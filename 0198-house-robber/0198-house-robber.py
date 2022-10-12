519963e90c1efb5ef31757b801cf68e08a8c6208
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-12 10:29:45 +0900

    Time: 58 ms (44.90%), Space: 13.8 MB (97.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,7 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        p0, p1 = 0, 0
+        result = [0,0]
         for m in nums:
-            p1, p0 = max(p0 + m, p1), p1
-            print(p1,p0)
-        return p1
+            result.append(max(result[-2]+m,result[-1]))
+        return result[-1]
