0b9ed45058a14e74e69ccc21ef3452f60a525eb4
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-18 12:34:15 +0900

    Time: 49 ms (91.94%), Space: 14.1 MB (28.81%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def change(x):
+            lst = []
+            tmp = 0
+            curr = x[0]
+            for i in range(len(x)):
+                if curr != x[i]:
+                    lst.extend((str(tmp),curr))
+                    curr = x[i]
+                    tmp = 1
+                else:
+                    tmp += 1
+            lst.extend((str(tmp),curr))
+            return ''.join(lst)
+        
+        num = '1'
+        n-=1
+        while n:
+            num = change(num)
+            n -= 1
+        
+        return num
