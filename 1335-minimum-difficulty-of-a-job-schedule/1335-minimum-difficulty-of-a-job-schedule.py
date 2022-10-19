e13c0eb25ca1c556c280d4fda3c2b82e5e87ab89
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-19 16:25:58 +0900

    Time: 3487 ms (14.55%), Space: 22.2 MB (5.05%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,36 +1,43 @@
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-#         leng = len(jobDifficulty)
-#         if leng < d:
-#             return -1
-#         elif leng == d:
-#             return sum(jobDifficulty)
+        leng = len(jobDifficulty)
+        if leng < d:
+            return -1
+        elif leng == d:
+            return sum(jobDifficulty)
+        elif d == 1:
+            return max(jobDifficulty)
         
-#         dp = [[0 for _ in range(leng)] for i in range(d)]
+        dp = [[3000000 for _ in range(leng)] for i in range(d)]
         
-#         @lru_cache(maxsize = None)
-#         def maxi(i,j):
-#             return max(jobDifficulty[i:j+1]) if i+1<j else jobDifficulty[i]
+        @lru_cache(maxsize = None)
+        def maxi(i,j=leng-1):
+            return max(jobDifficulty[i:j+1]) if i<j else jobDifficulty[i]
         
-#         num = jobDifficulty[0]
-#         for i in range(leng):
-#             if num<jobDifficulty[i]:
-#                 num = jobDifficult[i]
-#             dp[0][i] = num
-#         # define first days.
-#         # In dp[0], each value of dp[0][i] = max(jobDifficulty[:i])
+        num = jobDifficulty[0]
+        for i in range(leng):
+            if num<jobDifficulty[i]:
+                num = jobDifficulty[i]
+            dp[0][i] = num
+        # define first days.
+        # In dp[0], each value of dp[0][i] = max(jobDifficulty[:i])
+        for i in range(1,d-1):
+            for j in range(i,leng):
+                for k in range(i-1,j):
+                    tmp = dp[i-1][k]+maxi(k+1,j)
+                    if dp[i][j] > tmp:
+                        dp[i][j] = tmp
+                        
+        for j in range(d-1,leng):
+            for k in range(d-2,j):
+                tmp = dp[-2][k]+maxi(k+1)
+                if dp[-1][j] > tmp:
+                    dp[-1][j] = tmp
         
-#         for i in range(1,d):
-#             for j in range(i,leng):
-#                 # print(mini(i,j),i,j)
-#                 num = min(dp[i-1][i:j]) if i != j else dp[i-1][i]
-#                 # print(num)
-#                 dp[i][j] = num+maxi(i,j)
-        
-#         for i in dp:
-#             print(i)
-        
-#         return min(dp[-1][d:])
+        # for i in dp:
+        #     print(i)
+            
+        return min(dp[-1][d:])
 
 # ex1. 654321, 2
 # [6,6,6,6,6,6]
@@ -46,41 +53,44 @@ class Solution:
 # [0,11,10,9,8,7]
 # [0,0,15,13,11,9]
 
+#ex4. [380,302,102,681,863,676,243,671,651,612,162,561,394,856,601,30,6,257,921,405,716,126,158,476,889,699,668,930,139,164,641,801,480,756,797,915,275,709,161,358,461,938,914,557,121,964,315], 10
+# --> 3807
+
 # ! others code.
-        prefixSum = [0]
-        for n in jobDifficulty:
-            prefixSum.append(prefixSum[-1]+n)
+#         prefixSum = [0]
+#         for n in jobDifficulty:
+#             prefixSum.append(prefixSum[-1]+n)
         
         
-        maxList = deque([jobDifficulty[-1]])
-        for i in range(len(jobDifficulty)-2, -1, -1):
-            maxList.appendleft(max(jobDifficulty[i], maxList[0]))
+#         maxList = deque([jobDifficulty[-1]])
+#         for i in range(len(jobDifficulty)-2, -1, -1):
+#             maxList.appendleft(max(jobDifficulty[i], maxList[0]))
         
 		
-        memo = {}
+#         memo = {}
         
-        @lru_cache(maxsize = None)
-        def dp(i, k):
-            if (i,k) in memo: return memo[(i,k)]
-            if k == 1: return maxList[i]
-            if len(jobDifficulty)-i < k: return -1
-            if len(jobDifficulty)-i == k: 
-                memo[(i,k)] = prefixSum[-1]-prefixSum[i]
-                return memo[(i,k)]
+#         @lru_cache(maxsize = None)
+#         def dp(i, k):
+#             if (i,k) in memo: return memo[(i,k)]
+#             if k == 1: return maxList[i]
+#             if len(jobDifficulty)-i < k: return -1
+#             if len(jobDifficulty)-i == k: 
+#                 memo[(i,k)] = prefixSum[-1]-prefixSum[i]
+#                 return memo[(i,k)]
             
-            ans = float('inf')
-            cur_max = jobDifficulty[i]
+#             ans = float('inf')
+#             cur_max = jobDifficulty[i]
             
-            for j in range(i, len(jobDifficulty)-1):
-                cur_max = max(cur_max, jobDifficulty[j])
-                res = dp(j+1, k-1)
-                if res == -1: break
-                ans = min(ans, res+cur_max)
+#             for j in range(i, len(jobDifficulty)-1):
+#                 cur_max = max(cur_max, jobDifficulty[j])
+#                 res = dp(j+1, k-1)
+#                 if res == -1: break
+#                 ans = min(ans, res+cur_max)
                 
-            memo[(i,k)] = ans
-            return ans
-        x = dp(0,d)
+#             memo[(i,k)] = ans
+#             return ans
+#         x = dp(0,d)
+
+#         dp.cache_clear()
         
-        dp.cache_clear()
-        
-        return x
+#         return x
