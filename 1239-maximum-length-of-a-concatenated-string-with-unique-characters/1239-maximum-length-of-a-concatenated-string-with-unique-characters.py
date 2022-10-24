062594e40d63a7d0e916169c50e76d7b31c27cff
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-24 13:39:02 +0900

    Time: 919 ms (17.49%), Space: 13.8 MB (90.64%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,37 +1,56 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        # MLE.. 사실 당연.. 은 아닌가?
-        nums = []
-        for i in range(len(arr)):
-            tmp = 0
-            for j in range(len(arr[i])):
-                num = 2**(ord(arr[i][j])-97)
-                if tmp&num:
-                    tmp = 0
-                    break
-                tmp += num
-            if tmp:
-                nums.append(tmp)
+        l,r=0,0
+        s=""
+        res=0
+        def knapsack(path,arr,index):
+            a,b=len(path),len(Counter(path))
+            if a==b:
+                nonlocal res
+                res=max(res,len(path))
+            elif a!=b:
+                return
+            if index>=len(arr):return
+            knapsack(path+arr[index],arr,index+1)
+            knapsack(path,arr,index+1)
+        knapsack("",arr,0)
+        return res
+# others solution.
+
+# class Solution:
+#     def maxLength(self, arr: List[str]) -> int:
+#         # MLE.. 사실 당연.. 은 아닌가?
+#         nums = []
+#         for i in range(len(arr)):
+#             tmp = 0
+#             for j in range(len(arr[i])):
+#                 num = 2**(ord(arr[i][j])-97)
+#                 if tmp&num:
+#                     tmp = 0
+#                     break
+#                 tmp += num
+#             if tmp:
+#                 nums.append(tmp)
         
-        res = 0
-        Q = deque(nums)
-        nex = set()
-        while True:
-            while Q:
-                x = Q.popleft()
+#         res = 0
+#         Q = deque(nums)
+#         nex = set()
+#         while True:
+#             while Q:
+#                 x = Q.popleft()
 
-                check = False
-                for i in range(len(nums)):
-                    if not x&nums[i]:
-                        check = True
-                        nex.add(x^nums[i])
+#                 check = False
+#                 for i in range(len(nums)):
+#                     if not x&nums[i]:
+#                         check = True
+#                         nex.add(x^nums[i])
 
-                if not check:
-                    cnt = bin(x).count('1')
-                    if res<cnt:
-                        res = cnt
-            if not nex:
-                return res
+#                 if not check:
+#                     cnt = bin(x).count('1')
+#                     if res<cnt:
+#                         res = cnt
+#             if not nex:
+#                 return res
             
-            Q = deque(nex)
-            nex = set()
+#             Q = deque(nex)
+#             nex = set()
