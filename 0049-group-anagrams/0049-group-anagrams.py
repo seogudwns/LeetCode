b6c2ab9ee99da0a6b1c046ee7baa550046f366b8
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-10-28 14:22:32 +0900

    Time: 186 ms (67.00%), Space: 17.1 MB (96.53%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = dict()
+        for i in strs:
+            x = ''.join(sorted(i))
+            if x not in dic:
+                dic[x] = []
+            dic[x].append(i)
+        
+        return list(dic.values())
