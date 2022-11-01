18eaf1d9852fc9256df0100571802f7dcccb0d9e
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-11-01 18:45:39 +0900

    Time: 4083 ms (5.01%), Space: 13.9 MB (95.49%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates = sorted(candidates)
+        if candidates[0] > target:
+            return []
+        
+        res = []
+        
+        Q = deque([[]])
+        while Q:
+            x = Q.pop()
+            curr = sum(x)
+            if curr> target:
+                continue
+            elif curr == target:
+                tmp = sorted(x)
+                if tmp not in res:
+                    res.append(tmp)
+                continue
+                
+            for i in candidates:
+                Q.append(x+[i])
+        
+        return res
