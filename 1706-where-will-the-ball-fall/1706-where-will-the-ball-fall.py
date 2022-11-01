4a50c5838a37e86cc575f898f188cf2b3b34e026
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-11-01 14:40:19 +0900

    Time: 199 ms (95.71%), Space: 14.5 MB (30.84%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        m,n = len(grid),len(grid[0])
+        def check(i,j):
+            if i == m:
+                return j
+            if grid[i][j] == 1:
+                if j == n-1 or grid[i][j+1] == -1:
+                    return -1
+                else:
+                    return check(i+1,j+1)
+            
+            else:
+                if j == 0 or grid[i][j-1] == 1:
+                    return -1
+                else:
+                    return check(i+1,j-1)
+        
+        res = [check(0,i) for i in range(n)]
+        return res
