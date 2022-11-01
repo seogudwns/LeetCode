9722f2a9331ce773658d0f1c0010dd033f30449f
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-11-01 14:59:26 +0900

    Time: 58 ms (63.95%), Space: 13.9 MB (76.34%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        # print(2**31 - 1) # == 2147483648 - 1
+        if abs(dividend)<abs(divisor):
+            return 0
+        
+        if dividend*divisor>0:
+            return min(2147483647,abs(dividend)//abs(divisor))
+        
+        return -(min(2147483648,abs(dividend)//abs(divisor)))
