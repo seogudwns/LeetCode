f05fb248db13ef6cbcc1321939a2cd7584a1e1ec
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-11-03 22:53:40 +0900

    Time: 96 ms (76.85%), Space: 14 MB (59.87%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        for i in range(len(candidates)-1,-1,-1):
+            if candidates[i]>target:
+                del candidates[i]
+                
+        candidates = sorted(candidates,reverse = True)
+        leng = len(candidates)
+        res = []
+        def bt(start,target,lst):
+            if target<0:
+                return
+            elif target == 0:
+                lst = sorted(lst)
+                if lst not in res:
+                    res.append(lst)
+                return
+            
+            for i in range(start,leng):                    
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                lst.append(candidates[i])
+                bt(i+1,target-candidates[i],lst)
+                lst.pop()
+            
+            return
+        bt(0,target,[])
+        return res
