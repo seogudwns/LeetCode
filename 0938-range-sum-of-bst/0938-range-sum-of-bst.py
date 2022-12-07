f0b0d87a7436a190cbf970d049d8f3059a8a3cb3
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-12-07 10:19:19 +0900

    Time: 728 ms (12.85%), Space: 23 MB (92.23%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -3,13 +3,29 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right
+# #         self.right = right
+# class Solution:
+#     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         res = 0
+#         Q = deque([root])
+#         while Q:
+#             x = Q.popleft()
+#             if x.left:
+#                 Q.append(x.left)
+#             if x.right:
+#                 Q.append(x.right)
+            
+#             if low<=x.val<=high:
+#                 res += x.val
+        
+#         return res
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         res = 0
-        Q = deque([root])
+        Q = [root]
         while Q:
-            x = Q.popleft()
+            x = Q.pop()
             if x.left:
                 Q.append(x.left)
             if x.right:
