bcb64c5feabccd06aa255981c49ff4287fed9fe0
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-12-18 09:55:05 +0900

    Time: 2314 ms (62.79%), Space: 28.8 MB (51.76%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,14 +2,19 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         leng = len(temperatures)
         dp = [0]*len(temperatures)
-        check = [[] for _ in range(101)] # key:val = temp:idx
+        check = [[] for _ in range(101)]
+        consider = set() # check를 하나씩 다 도는게 싫어서 씀.
         for i in range(leng):
             temp = temperatures[i]
-            for j in range(1,temp):
+            for j in list(consider):
+                if j>=temp:
+                    continue
                 if check[j]:
                     for k in check[j]:
                         dp[k] = i-k
                     check[j].clear()
+                    consider.discard(j)
             check[temp].append(i)
+            consider.add(temp)
         
         return dp
