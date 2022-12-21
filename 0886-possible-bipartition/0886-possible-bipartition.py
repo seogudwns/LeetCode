4c0f445123d0d5aae1c5f391e0b32a3d4a76c2c7
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2022-12-21 12:59:16 +0900

    Time: 962 ms (70.19%), Space: 20 MB (80.54%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,26 +1,30 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        visited=[0]*n
-        group=[-1]*n
-        adj=[[] for _ in range(n)]
-        for i,j in dislikes:
-            adj[i-1].append(j-1)
-            adj[j-1].append(i-1)
-            
-        for k in range(n):
-            if visited[k]==0:
-                lst=[[k,0]]
-                visited[k]=1
-                group[k]=0
-                while lst:
-                    x,c=lst.pop(0)
-                    for i in adj[x]:
-                        if visited[i]==0:
-                            lst.append([i,(c+1)%2])
-                            visited[i]=1
-                            group[i]=(c+1)%2
-                        else:
-                            if group[i]!=(c+1)%2:
-                                return False
+        if not dislikes:
+            return True
         
-        return True
+        bigp = [-1 for _ in range(n+1)]
+        bigp[dislikes[0][0]] = 0
+        dislikes = deque(dislikes)
+        tmp = deque()
+        while True:
+            while dislikes:
+                x,y = dislikes.popleft()
+                
+                if bigp[x] != -1 and bigp[y] != -1:
+                    if bigp[x]%2 == bigp[y]%2:
+                        return False
+                elif bigp[x] != -1:
+                    bigp[y] = bigp[x] + 1
+                elif bigp[y] != -1:
+                    bigp[x] = bigp[y] + 1
+                else:
+                    tmp.append((x,y))
+                    
+            if not tmp:
+                print(bigp)
+                return True
+            if bigp[tmp[0][0]] == -1:
+                bigp[tmp[0][0]] = 0
+            dislikes = tmp
+            tmp = deque()
