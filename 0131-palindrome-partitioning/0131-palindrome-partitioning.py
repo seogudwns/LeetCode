2be5458c42b83e41e337eb05206b9453963da8dd
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-01-22 09:55:32 +0900

    Time: 666 ms (82.30%), Space: 28.8 MB (96.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,4 +1,25 @@
 class Solution:
+#     def check_pel(self,ss):
+#         n = len(ss)//2
+#         for i in range(n):
+#             if ss[i] != ss[-i-1]:
+#                 return False
+#         return True
+    
+#     def partition(self, s: str) -> List[List[str]]:
+#         Q = deque([([],s)])
+#         ans = []
+#         while Q:
+#             curr,resi = Q.popleft()
+#             if not resi:
+#                 ans.append(curr)
+#                 continue
+            
+#             for i in range(1,len(resi)+1):
+#                 if self.check_pel(resi[:i]):
+#                     Q.append((curr+[resi[:i]],resi[i:]))
+                    
+#         return ans
     def check_pel(self,ss):
         n = len(ss)//2
         for i in range(n):
@@ -6,17 +27,19 @@ class Solution:
                 return False
         return True
     
+    def dfs(self,ss,curr):
+        if not ss:
+            self.ans.append(curr)
+            return
+        
+        n = len(ss)
+        for j in range(1,n+1):
+            if self.check_pel(ss[:j]):
+                self.dfs(ss[j:],curr+[ss[:j]])
+                
+    
     def partition(self, s: str) -> List[List[str]]:
-        Q = deque([([],s)])
-        ans = []
-        while Q:
-            curr,resi = Q.popleft()
-            if not resi:
-                ans.append(curr)
-                continue
-            
-            for i in range(1,len(resi)+1):
-                if self.check_pel(resi[:i]):
-                    Q.append((curr+[resi[:i]],resi[i:]))
-                    
-        return ans
+        self.ans = []
+        self.dfs(s,[])
+        
+        return self.ans
