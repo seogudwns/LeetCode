e9b70aaf04967982d7cd03faae7b0f821cf151b4
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-01-26 22:35:43 +0900

    Time: 1195 ms (5.02%), Space: 15.7 MB (11.74%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,26 @@
+class Solution:
+    def bfs(self,curr,ks,char):
+        if self.check[ks][curr]<=char or self.ans < char:
+            return
+        self.check[ks][curr] = char
+        # print(curr,ks,char)
+        if curr == self.dst:
+            self.ans = min(self.ans,char)
+            return
+        if ks == self.k: return
+        
+        for i in self.mv[curr]:
+            self.bfs(i,ks+1,char+self.charges[(curr,i)])
+        
+    
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        self.charges,self.mv,self.ans,self.k,self.dst = defaultdict(int),defaultdict(set),10**6+1,k,dst
+
+        for i,j,char in flights:
+            self.charges[(i,j)] = char
+            self.mv[i].add(j)
+        self.check = [[float('inf') for _ in range(n)] for _ in range(k+1)]
+        
+        self.bfs(src,-1,0)
+        
+        return self.ans if self.ans != 10**6+1 else -1
