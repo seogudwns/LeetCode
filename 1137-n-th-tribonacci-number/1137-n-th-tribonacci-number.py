ae8a1c97ecfffb23ae2d71577c024140d1c79082
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-01-30 13:17:57 +0900

    Time: 34 ms (60.55%), Space: 13.9 MB (10.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0: return 0
-        elif n < 3: return 1
         A,X = 2**32,[0,1,1]
+        if n<3: return X[n]
         for i in range(n-2): X.append(sum(X[-3:])%A)
         return X[-1]
