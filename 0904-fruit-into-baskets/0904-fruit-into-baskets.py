64d1a71eeac172f2ae25a1563eea12dec9022fd6
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-02-08 00:47:05 +0900

    Time: 817 ms (96.46%), Space: 20.4 MB (28.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -9,26 +9,12 @@ class Solution:
                 cnts.append((curr,cnt))
                 curr,cnt = fruits[i],1
         cnts.append((curr,cnt))
-        # print(cnts)
+        
         if len(cnts) == 1: return cnts[0][1]
         ans,cnt,basket = cnts[0][1]+cnts[1][1],cnts[0][1],{cnts[0][0],cnts[1][0]}
         for i in range(1,len(cnts)):
             bef = cnts[i][1]
             if cnts[i][0] in basket: cnt += bef
             else: ans,cnt,basket = max(ans,cnt),bef+cnts[i-1][1],{cnts[i-1][0],cnts[i][0]}
-            # print(i,ans,cnt,basket)
         
         return max(ans,cnt)
-        
-#         curr,start,bef,ans = fruits[:2],-1,0,2
-#         for i in range(2,len(fruits)):
-#             if fruits[i] in curr:
-#                 if fruits[i] == curr[0]: curr[:],bef = curr[::-1],i-1
-#             else: ans,start,bef,curr = max(ans,i-start),bef,i-1,[fruits[i-1],fruits[i]]
-#             print(ans,i,curr,start,bef)
-        
-#         return max(ans,i-start)
-    
-#     [3,3,3,1,2,1,1,2,3,3,4]
-#     [3,3,3,3,1,1,1,1,1]
-#     [3,3,3,3,1,1,1,1,1,3,4]
