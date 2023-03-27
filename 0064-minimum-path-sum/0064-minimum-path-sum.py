55874ba9e8e641759e8768fb042f43df23d1c576
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-03-27 10:48:42 +0900

    Time: 98 ms (73.26%), Space: 15.7 MB (46.72%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -5,8 +5,6 @@ class Solution:
         for i in range(1,n): grid[i][0] += grid[i-1][0]
         for i in range(1,m): grid[0][i] += grid[0][i-1]
         
-        if n == 1 or m == 1: return grid[-1][-1]
-        
         for i in range(1,n):
             for j in range(1,m):
                 grid[i][j] += min(grid[i-1][j],grid[i][j-1])
