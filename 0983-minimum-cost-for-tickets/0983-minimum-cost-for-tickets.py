a9ef95cfbc3bce1eaae76d52ffd539507d076ba4
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-03-28 09:59:29 +0900

    Time: 44 ms (76.66%), Space: 13.8 MB (83.75%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,23 +1,11 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         last = days[-1]
-        chk,days = [0 for _ in range(last)],set(days)
+        chk,days = [0 for _ in range(last+1)],set(days)
         o,s,m = costs
         
-        for i in range(last):
+        for i in range(1,last+1):
             if i not in days: chk[i] = chk[i-1]
             else: chk[i] = min(chk[i-1]+o,chk[max(0,i-7)]+s,chk[max(0,i-30)]+m)
-                
+            
         return chk[-1]
-        
-class Solution:
-    def mincostTickets(self, days, costs):
-    
-        dp = [0] * 366
-        travel_days = set(days)
-        for i in range(1, 366):
-            if i not in travel_days:
-                dp[i] = dp[i-1]
-            else:
-                dp[i] = min(dp[i-1] + costs[0], dp[max(0, i-7)] + costs[1], dp[max(0, i-30)] + costs[2])
-        return dp[365]
