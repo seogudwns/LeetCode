8597cf6a5a0fbe1f0cf1a33fe40f21e493596252
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-05-22 11:10:30 +0900

    Time: 123 ms (24.41%), Space: 21.1 MB (30.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        x = Counter(nums)
-        return sorted(x,key = lambda y:-x[y])[:k]
+        return sorted(x:=Counter(nums),key = lambda y:-x[y])[:k]
         
