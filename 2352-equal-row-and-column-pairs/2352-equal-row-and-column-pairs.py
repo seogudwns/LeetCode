83ca7d69a917ef3abf83d9d85f864f5bd92d907d
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-06-13 16:59:56 +0900

    Time: 1300 ms (12.77%), Space: 21.5 MB (56.95%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,4 +1,3 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        X = Counter(tuple(i) for i in grid)
-        return sum(X[tuple(grid[i][j] for i in range(len(grid)))] for j in range(len(grid)))    
+        return sum(Counter(tuple(i) for i in grid)[tuple(grid[i][j] for i in range(len(grid)))] for j in range(len(grid)))    
