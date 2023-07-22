293a88a4b0ca85f1953c836d70be5a3ac77a6199
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-07-22 14:28:27 +0900

    Time: 209 ms (84.50%), Space: 25.5 MB (40.68%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -0,0 +1,32 @@
+class Solution:
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        
+        Q,N = deque([(row,column)]),[0 for _ in range(k+1)]
+        rot = [(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1),(-2,1),(-1,2)]
+        
+        @lru_cache(None)
+        def knightcheck(ro,co,mo):
+            if mo == k:
+                return 1
+            ans = 0
+            for i,j in rot:
+                nro,nco = ro+i,co+j
+                if 0<=nro<n and 0<=nco<n:
+                    ans+=1/8 * knightcheck(nco,nro,mo+1)
+                        
+            return ans
+        
+        return knightcheck(row,column,0)
+        
+#         prob = 8
+#         ans = 1
+#         for i in range(k):
+#             ans -= N[i]/prob
+#             prob*=8
+        
+#         return ans
+
+# 8
+# 30
+# 6
+# 4
