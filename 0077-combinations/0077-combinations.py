7e56c116fcd39b0757fbe19312dc0464e8d70655
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-08-01 12:55:05 +0900

    Time: 235 ms (80.71%), Space: 18.1 MB (90.06%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,3 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return [list(i) for i in combinations([i for i in range(1,n+1)],k)]
+        # return [list(i) for i in combinations([i for i in range(1,n+1)],k)]
+        ans = []
+        for i in range(1,1<<n):
+            if bin(i).count('1')==k:
+                x = bin(i)[::-1]
+                tmp = []
+                for i in range(len(x)):
+                    if x[i] == '1':
+                        tmp.append(i+1)
+                ans.append(tmp)
+                
+        return ans
