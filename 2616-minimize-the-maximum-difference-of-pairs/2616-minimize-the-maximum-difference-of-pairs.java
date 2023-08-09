1db4a984910028d13519f31e5de4b57eaa02af88
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-08-09 09:43:41 +0900

    Time: 16 ms (99.38%), Space: 59 MB (88.75%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.java
@@ -1,19 +1,20 @@
-class Solution:
-    def minimizeMax(self, nums: List[int], p: int) -> int:
-        nums = sorted(nums)
-        n = len(nums)
-        left, right = 0, nums[-1] - nums[0]
-        while left < right:
-            mid = (left + right) // 2
-            k = 0
-            i = 1
-            while i < n:
-                if nums[i] - nums[i - 1] <= mid:
-                    k += 1
-                    i += 1
-                i += 1
-            if k >= p:
-                right = mid
-            else:
-                left = mid + 1
-        return left
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int n = nums.length, left = 0, right = nums[n - 1] - nums[0];
+        while (left < right) {
+            int mid = (left + right) / 2, k = 0;
+            for (int i = 1; i < n && k < p; ++i) {
+                if (nums[i] - nums[i - 1] <= mid) {
+                    k++;
+                    i++;
+                }
+            }
+            if (k >= p)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+}
