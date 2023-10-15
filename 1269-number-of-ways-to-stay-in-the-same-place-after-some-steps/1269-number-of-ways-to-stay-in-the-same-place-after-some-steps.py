2a597551fac57f7f61c135f6839ce07e30f4ad96
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-10-15 15:37:37 +0900

    Time: 695 ms (10.55%), Space: 94.2 MB (33.33%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -0,0 +1,18 @@
+class Solution:
+    @lru_cache(None)
+    def chk(self,res,loc):
+        if res == 0:
+            return 1 if loc == 0 else 0
+        if loc == 0: 
+            return (self.chk(res-1,loc)+self.chk(res-1,loc+1))%self.MOD
+        elif loc == self.arrLen:
+            return (self.chk(res-1,loc-1)+self.chk(res-1,loc))%self.MOD
+        else:
+            return (self.chk(res-1,loc-1)+self.chk(res-1,loc)+self.chk(res-1,loc+1))%self.MOD
+
+    
+    def numWays(self, steps: int, arrLen: int) -> int:
+        if arrLen == 1: return 1
+        self.MOD,self.arrLen = 10**9+7,arrLen-1
+        return self.chk(steps,0)
+        
