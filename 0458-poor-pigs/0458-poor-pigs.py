2325f7abb3601e48cc7cfab5ec1aa8cc49201ece
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2023-10-29 12:03:45 +0900

    Time: 31 ms (92.80%), Space: 16.2 MB (36.00%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.py
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -3,8 +3,10 @@ class Solution:
         if buckets == 1: return 0
         max_time = minutesToTest / minutesToDie + 1
         req_pigs = 1
+        unit = max_time
         
-        while max_time**req_pigs < buckets:
+        while max_time < buckets:
+            max_time*=unit
             req_pigs += 1
 
         return req_pigs
