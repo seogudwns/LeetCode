76963d9c62fdbc4752e97b83a2509b6812131f04
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2024-04-03 10:24:34 +0900

    Time: 430 ms (96.12%), Space: 16.6 MB (26.43%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,22 +1,43 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtrack(i, j, k):
-            if k == len(word):
-                return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
-                return False
-            
-            temp = board[i][j]
-            board[i][j] = ''
-            
-            if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
-                return True
-            
-            board[i][j] = temp
+        rot = [(1,0),(0,1),(-1,0),(0,-1)]
+        m,n = len(board),len(board[0])
+        leng = len(word)
+        if m*n<leng:
             return False
+        board_words = Counter(sum(board,[]))
+        for i in word:
+            if i not in board_words:
+                return False
+
+            board_words[i] -= 1
+            if board_words[i] == -1:
+                return False
+
+        first = word[0]
+        def word_check(cc,cr,curr):
+            if curr == leng:
+                return True
+            # print(cc,cr,curr,word[curr])
+
+            for c,r in rot:
+                new_c,new_r = cc+c,cr+r
+                if 0<=new_c<m and 0<=new_r<n and board[new_c][new_r] == word[curr]:
+                    tmp = board[new_c][new_r]
+                    board[new_c][new_r] = '.'
+                    if word_check(new_c,new_r,curr+1):
+                        return True
+                    board[new_c][new_r] = tmp
+
+            return False
+
+        for c in range(m):
+            for r in range(n):
+                if board[c][r] == first:
+                    tmp = board[c][r]
+                    board[c][r] = '.'
+                    if word_check(c,r,1):
+                        return True
+                    board[c][r] = tmp
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if backtrack(i, j, 0):
-                    return True
         return False
