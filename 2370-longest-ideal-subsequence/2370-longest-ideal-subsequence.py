ae35eba33a1a43ae2efe190e288f7b4c6f41488a
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2024-04-26 08:59:26 +0900

    Time: 286 ms (97.08%), Space: 17.3 MB (68.61%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -2,9 +2,9 @@ class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
 
 
-        l = [0] * 128
+        l = [0] * 52
         for c in s:
-            i = ord(c)
+            i = ord(c)-71
             l[i] = max(l[i - k : i + k + 1]) + 1
         return max(l)
             
