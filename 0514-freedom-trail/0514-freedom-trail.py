475282e4f2f9e4787fcf3c17f0bf71677af25fa7
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2024-04-27 17:49:16 +0900

    Time: 169 ms (43.53%), Space: 16.6 MB (76.47%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -1,22 +1,16 @@
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
         n,m = len(ring),len(key)
-        def calc(lst,curr):
-            return min(lst[i]+min(abs(curr-i),n-abs(curr-i)) for i in range(n) if lst[i] != 0)
+        def calc(lst,curr): return min(lst[i]+min(abs(curr-i),n-abs(curr-i)) for i in range(n) if lst[i] != 0)
 
         dp = [[0]*n for _ in range(m)]
         for i in range(n):
             if ring[i] == key[0]:
                 dp[0][i] = 1+min(i,n-i)
 
-        print(dp[0])
-        
         for i in range(1,m):
             for j in range(n):
                 if ring[j] == key[i]:
                     dp[i][j] = 1+calc(dp[i-1],j)
-        
-#         for i in dp:
-#             print(i)
         
         return min(i for i in dp[-1] if i != 0)
