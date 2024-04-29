8c447020918f49acdb196e319a6409fb230ffecf
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2024-04-29 13:44:57 +0900

    Time: 610 ms (14.01%), Space: 31 MB (81.85%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,8 +1,3 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         return bin(reduce(lambda curr,x: curr^x, nums,k)).count("1")
-#         curr = k
-#         for i in nums:
-#             curr ^= i
-        
-#         return bin(curr).count("1")
