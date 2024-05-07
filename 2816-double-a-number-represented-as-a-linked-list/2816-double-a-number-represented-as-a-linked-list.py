c1aac5a95469bbd4592e77a4361d6ca160f718f4
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2024-05-07 13:55:33 +0900

    Time: 206 ms (98.75%), Space: 19.3 MB (86.07%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
@@ -6,17 +6,38 @@
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head.val == 0 and not head.next: return ListNode(val=0)
-        num = 0
-        while head:
-            num = num*10 + head.val
-            head = head.next
-        num *= 2
-        curr = False
-        while num:
-            x,y = divmod(num,10)
-            if x:
-                curr = ListNode(val=y,next=curr) if curr else ListNode(val=y)
-                num = x
+        
+        curr = head
+        bef = head
+        if head.val>=5: 
+            head = ListNode(val=1,next=head)
+            curr = head.next
+            curr.val = curr.val*2-10
+            bef = head.next
+            curr = curr.next
+            
+        while curr:
+            if curr.val>=5:
+                bef.val += 1
+                curr.val = 2*curr.val-10
             else:
-                return ListNode(val=y) if not curr else ListNode(val=y,next=curr)
+                curr.val = 2*curr.val
+            bef,curr = curr,curr.next
+        
+        return head
+
+#         num = 0
+#         while head:
+#             num = num*10 + head.val
+#             head = head.next
+#         num *= 2
+#         curr = False
+        
+#         while num:
+#             x,y = divmod(num,10)
+#             if x:
+#                 curr = ListNode(val=y,next=curr) if curr else ListNode(val=y)
+#                 num = x
+#             else:
+#                 return ListNode(val=y) if not curr else ListNode(val=y,next=curr)
             
