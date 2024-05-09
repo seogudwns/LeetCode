e6fe3b615a21e0622b981e9d4cbb26d988d124cc
HJ_SEO <38065726+seogudwns@users.noreply.github.com>
2024-05-09 10:16:40 +0900

    Time: 782 ms (86.71%), Space: 43.5 MB (33.89%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -3,5 +3,6 @@ class Solution:
         happiness = sorted(happiness)
         ans = 0
         for i in range(k):
+            if happiness[-1]<=i: break
             ans = max(ans,ans+happiness.pop()-i)
         return ans
